feaf036d9dc3ee5d0aa4879af2f64f1d81e07bc3
Lena710 <85823557+Lena710@users.noreply.github.com>
2026-04-25 21:42:03 -0400

    Improved UX

--- src/index.js
+++ src/index.js
@@ -12,9 +12,13 @@ function generatePoem(event) {
 
     let topicElement = document.querySelector('#user-topic');
 
+    let poemElement = document.querySelector('#poem-element');
+    poemElement.classList.remove('hidden');
+    poemElement.innerHTML = `<div class='blink'>Generating a poem about ${topicElement.value}</div>`;
+
     let apiKey = 'b6bd5oaet2f63fa47b510ab2e1146be4';
     let prompt = `Write a poem about ${topicElement.value}. Separate each line with <br/>`;
-    let context = 'You are a French poet, who writes 4-line poems about any subject. Sign with "Shecodes AI" at the bottom ';
+    let context = 'You are a French poet, who writes 4-line poems about any subject. Sign with "SheCodes AI" inside <strong> element at the bottom ';
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(displayPoem);
